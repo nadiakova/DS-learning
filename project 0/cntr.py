6fe2f91698372860f2533210555012160f56f0d7
nadiakova <nadin_tiger@mai.ru>
2021-11-30 10:20:44 +0300

    21-11-30 1

---
+++ project 0/cntr.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+dict_object = dict()
+defaultdict_object = defaultdict(int)
+ 
+#print(type(dict_object))
+# <class 'dict'>
+#print(type(defaultdict_object))
+# <class 'collections.defaultdict'>
+
+#print(dict_object)
+# {}
+print(defaultdict_object)
+# defaultdict(None, {})
